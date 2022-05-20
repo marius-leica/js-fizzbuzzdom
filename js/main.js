e02f5ed992589112c04c658a680504d2be6353b3
Marius L <mariusleica@gmail.com>
2022-05-20 17:32:12 +0200

    fizzbuzz repair

--- js/main.js
+++ js/main.js
@@ -8,19 +8,31 @@
 const result = document.querySelector(".square-container");
 
 for (let i = 1; i <= 100; i++) {
-  const testo = `${i}`; // creiamo una variabile che contiene il numero da stampare
+  let testo = `${i}`; // creiamo una variabile che contiene il numero da stampare
+  let color;
+  if (i % 3 === 0 && i % 5 === 0) {
+    testo = "FizzBuzz";
+    color = "bg-rose";
+    console.log(i + " " + testo);
 
-  if (i % 3 === 0) {
-    console.log(i + " Fizz"); // stampiamo il numero se è multiplo di 3
-    result.innerHTML += `<div class="square bg-green"> Fizz</div>`; // appendiamo il numero alla pagina
+    // result.innerHTML += `<div class="square bg-rose">FizzBuzz</div>`;
+  } else if (i % 3 === 0) {
+    testo = "Fizz";
+    color = "bg-green";
+    console.log(i + " " + testo); // stampiamo il numero se è multiplo di 3
+
+    // result.innerHTML += `<div class="square bg-green"> Fizz</div>`; // appendiamo il numero alla pagina
   } else if (i % 5 === 0) {
-    console.log(i + " Buzz");
-    result.innerHTML += `<div class="square bg-yellow">Buzz</div>`;
-  } else if (i % 3 === 0 && i % 5 === 0) {
-    console.log(i + " FizzBuzz");
-    result.innerHTML += `<div class="square bg-rose">fizzBuzz</div>`;
+    testo = "Buzz";
+    color = "bg-yellow";
+    console.log(i + " " + testo);
+
+    // result.innerHTML += `<div class="square bg-yellow">Buzz</div>`;
   } else {
+    color = "bg-blue";
     console.log(i);
-    result.innerHTML += `<div class="square bg-blue">${testo}</div>`;
+
+    // result.innerHTML += `<div class="square bg-blue">${testo}</div>`;
   }
+  result.innerHTML += `<div class="square ${color}">${testo}</div>`;
 }
